e5b54c9322db3733b531792b5cd5fb7af10a834f
ramonsimon <ramonsimon8@gmail.com>
2022-04-05 11:21:22 +0200

    Articles are now connected to db

---
+++ resources/views/components/forms/tinymce-editor.blade.php
@@ -0,0 +1,3 @@
+<form method="post">
+    <textarea id="myeditorinstance"></textarea>
+</form>
---
+++ resources/views/components/head/tinymce-config.blade.php
@@ -0,0 +1,9 @@
+<script src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
+        plugins: 'code table lists image',
+        toolbar: 'undo redo | formatselect| bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table',
+
+    });
+</script>

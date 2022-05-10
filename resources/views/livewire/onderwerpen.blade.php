89055b6deb3067bc66bb2829e83d52c9416609e1
ZaroxJH <stefan.hoekstra02@gmail.com>
2022-05-10 12:08:40 +0200

    Styling bugs fixed in onderwerpen

--- resources/views/livewire/onderwerpen.blade.php
+++ resources/views/livewire/onderwerpen.blade.php
@@ -17,12 +17,12 @@
 
         <div class="flex bg-white mt-5 rounded-xl items-center flex-col overflow-auto pb-10 px-7 w-1/2">
 
-            <div class="flex mb-5 mt-5 items-center">
+            <div class="flex flex-wrap mb-5 mt-5 items-center">
                 <div class="text-black uppercase font-bold text-center text-xl mt-5 mb-3 mr-56">
                     Onderwerpen
                 </div>
 
-                <div class="flex mb-5 mt-5 mr-5">
+                <div class="mb-5 mt-5 mr-5">
                     <select name="topic" id="topic"
                             class=" rounded-lg flex-1 appearance-none border border-amber-300 w-48 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
                         <option>Filter</option>
@@ -30,7 +30,7 @@
                 </div>
 
                 @role('admin')
-                    <div class="flex mb-5 mt-5">
+                    <div class="mb-5 mt-5">
                             <button wire:click='$emit("openModal", "onderwerp-toevoegen")'
                                     class="py-2 px-4  bg-amber-500 hover:bg-amber-700 text-white w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none rounded-lg ">
                                 Onderwerp toevoegen
@@ -40,12 +40,12 @@
 
             </div>
 
-            <div class="flex flex-wrap justify-between overflow-hidden overflow-y-auto">
+            <div class="flex flex-wrap overflow-hidden overflow-y-auto">
                 @if(!$topics->isEmpty())
                 @foreach($topics as $topic)
                 <div class="mb-5">
                     <button type="button"
-                            class="w-52 h-16 bg-amber-500 hover:bg-amber-700 text-white w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none rounded-lg ">
+                            class="mx-2 w-52 h-16 bg-amber-500 hover:bg-amber-700 text-white w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none rounded-lg ">
                         {{$topic->name}}
                     </button>
 

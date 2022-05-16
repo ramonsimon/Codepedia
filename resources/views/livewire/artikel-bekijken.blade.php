cc68db66b2179da5e07e65046f3a993238b2b098
ZaroxJH <stefan.hoekstra02@gmail.com>
2022-05-16 11:17:46 +0200

    Reactie verwijderen en wijzigen

--- resources/views/livewire/artikel-bekijken.blade.php
+++ resources/views/livewire/artikel-bekijken.blade.php
@@ -87,7 +87,8 @@
                                     </div>
 
                                     <div class="flex flex-row items-center justify-center">
-                                        <div>
+                                        @if($comment->user->id == auth()->id())
+                                        <button wire:click='$emit("openModal", "reactie-wijzigen", {{ json_encode(["comment" => $comment, 'slug' => $article->slug]) }})'>
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
                                                  fill="gray">
                                                 <path
@@ -96,15 +97,17 @@
                                                       d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
                                                       clip-rule="evenodd"/>
                                             </svg>
-                                        </div>
-                                        <div>
+                                        </button>
+
+                                        <button wire:click='$emit("openModal", "reactie-verwijderen", {{ json_encode(["comment" => $comment, 'slug' => $article->slug]) }})'>
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" viewBox="0 0 20 20"
                                                  fill="gray">
                                                 <path fill-rule="evenodd"
                                                       d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                                                       clip-rule="evenodd"/>
                                             </svg>
-                                        </div>
+                                        </button>
+                                        @endif
                                     </div>
                                 </div>
                                 @empty

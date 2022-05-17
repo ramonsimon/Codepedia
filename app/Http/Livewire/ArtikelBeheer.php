6c7d2ca259f28d6dc9996f266511c2f1189f76b5
ZaroxJH <stefan.hoekstra02@gmail.com>
2022-05-17 11:48:00 +0200

    Laatste paar kleine updates

--- app/Http/Livewire/ArtikelBeheer.php
+++ app/Http/Livewire/ArtikelBeheer.php
@@ -2,18 +2,46 @@
 
 namespace App\Http\Livewire;
 
+use App\Http\Controllers\VotesController;
 use App\Models\Article;
+use App\Models\Topics;
+use Illuminate\Support\Facades\Session;
 use Livewire\Component;
+use Livewire\WithPagination;
+
 
 class ArtikelBeheer extends Component
 {
+    use WithPagination;
 
+    public $topic = 'all';
+    public $search;
 
+    public function getVotes($id)
+    {
+        $votes_controller = new VotesController();
+        return $votes_controller->getRating('Article', $id);
+    }
 
     public function render()
     {
-        return view('livewire.artikel-beheer',
-            ['articles' => Article::all(),
+
+        $search = '%' . $this->search . '%';
+
+        if (Session::has('topic')) {
+            $this->topic = Session::get('topic');
+        }
+
+        if ($this->topic == "all") {
+            $articles = Article::where('title', 'like', $search)->simplePaginate(8, ['*'], 'articlePage');
+        } else {
+            $articles = Article::where('title', 'like', $search )
+                ->where('topic_id', '=', $this->topic)->simplePaginate(8, ['*'], 'articlePage');
+        }
+
+        return view('livewire.artikel-beheer', [
+            'topics' => Topics::get(),
+            'articles' => $articles,
         ]);
     }
 }
--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -1,68 +1,73 @@
 
     <div class="flex flex-row justify-center">
-        <div class="space-y-6 my-6 pr-2" style="width: 600px; ">
-            <div class="mb-5">
+        <div class="mt-6 pr-2" style="width: 600px; ">
+            <div class="flex justify-center items-center">
+                <h1 class="font-bold text-xl">Nieuwste artikelen</h1>
+            </div>
+            <div class="mb-2">
                 {{ $articles->links() }}
             </div>
-            @if(!$articles->isEmpty())
-                @foreach($articles as $article)
-                    <div
-                        x-data
-                        @click="const ignores = ['button','a','svg','path'];
-                    if(!ignores.includes($el.tagName.toLowerCase())){
-                        window.location=$refs.link.href;
-                    }"
+            <div class="space-y-6 mb-2">
+                @if(!$articles->isEmpty())
+                    @foreach($articles as $article)
+                        <div
+                            x-data
+                            @click="const ignores = ['button','a','svg','path'];
+                        if(!ignores.includes($el.tagName.toLowerCase())){
+                            window.location=$refs.link.href;
+                        }"
 
-                        class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer"
-                    >
-                        <div class="border-r border-gray-100 px-5 py-8 flex items-center">
-                            <div class="text-center">
-                                <div class="font-semibold text-2xl">{{ $article->rating }}</div>
-                                <div class="text-gray-500">Votes</div>
-                            </div>
-
-                        </div>
-
-                        <div class="flex px-2 py-6">
-                            <div class="mx-4">
-                                <h4 class="text-xl font-semibold">
-                                    <a href="{{ route('artikel-bekijken', $article) }}" x-ref="link" class=hover:underline">{{$article->title}}</a>
-                                </h4>
-                                <div class=" mt-3 line-clamp-3">
-                                    {{$article->sub_description}}
+                            class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer"
+                        >
+                            <div class="border-r border-gray-100 px-5 py-8 flex items-center">
+                                <div class="text-center">
+                                    <div class="font-semibold text-2xl">{{ $this->getVotes($article->id) }}</div>
+                                    <div class="text-gray-500">Votes</div>
                                 </div>
 
-                                <div class="flex items-center justify-between mt-6">
-                                    <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                                        <div>{{ $article->created_at->diffForHumans() }}</div>
-                                        <div>&bull;</div>
-                                        <div>{{ $article->topic->name }}</div>
-                                        <div>&bull;</div>
-                                        <div class="text-gray-900">{{$article->comments->count()}} reacties</div>
+                            </div>
+
+                            <div class="flex px-2 py-6">
+                                <div class="mx-4">
+                                    <h4 class="text-xl font-semibold">
+                                        <a href="{{ route('artikel-bekijken', $article) }}" x-ref="link" class=hover:underline">{{$article->title}}</a>
+                                    </h4>
+                                    <div class=" mt-3 line-clamp-3">
+                                        {{$article->sub_description}}
                                     </div>
-                                    <!--<div class="flex items-center space-x-2">
-                                        <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
-                                        <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
-                                            <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
-                                            <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
-                                                <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
-                                                <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
-                                            </ul>
-                                        </button>
-                                    </div>-->
+
+                                    <div class="flex items-center justify-between mt-6">
+                                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                                            <div>{{ $article->created_at->diffForHumans() }}</div>
+                                            <div>&bull;</div>
+                                            <div>{{ $article->topic->name }}</div>
+                                            <div>&bull;</div>
+                                            <div class="text-gray-900">{{$article->comments->count()}} reacties</div>
+                                        </div>
+                                        <!--<div class="flex items-center space-x-2">
+                                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
+                                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
+                                                <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
+                                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
+                                                </ul>
+                                            </button>
+                                        </div>-->
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div> <!-- end idea-container -->
-                @endforeach
+                        </div> <!-- end idea-container -->
+                    @endforeach
 
-            @else
+                @else
 
-                <div class="flex justify-center text-center">
-                    <a>Er zijn nog geen artikelen</a>
-                </div>
+                    <div class="flex justify-center text-center">
+                        <a>Er zijn nog geen artikelen</a>
+                    </div>
 
-            @endif
+                @endif
+            </div>
         </div>
         <div class="relative z-0">
             <div class="ml-9 mt-6 z-0" style="width: 500px; height: 500px;">

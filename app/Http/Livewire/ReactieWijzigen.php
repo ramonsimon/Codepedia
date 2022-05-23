6f16a1b18dfd03a54f50e6ebbde8ee62a0d998d8
ZaroxJH <stefan.hoekstra02@gmail.com>
2022-05-23 15:22:29 +0200

    Reactie wijzigen fixed

--- app/Http/Livewire/ReactieWijzigen.php
+++ app/Http/Livewire/ReactieWijzigen.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\question_comments;
 use LivewireUI\Modal\ModalComponent;
 use App\Models\Comments;
 
@@ -10,6 +11,7 @@ class ReactieWijzigen extends ModalComponent
 
     public $slug;
     public $body;
+    public $type;
     public $comment;
 
     public static function modalMaxWidth(): string
@@ -28,21 +30,43 @@ class ReactieWijzigen extends ModalComponent
 
     public function submit()
     {
-        if (auth()->id() != $this->comment['user_id']) {
-            return redirect('/artikel/' . $this->slug);
+
+        if ($this->type == 'article') {
+
+            if (auth()->id() != $this->comment['user_id']) {
+                return redirect('/artikel/' . $this->slug);
+            }
+
+            $this->validate();
+
+            Comments::where('id', $this->comment['id'])->
+            update(['body' => $this->body]);
+
+            return redirect('/artikel/' . $this->slug)->with([
+                'title' => 'Gelukt!',
+                'message' => 'Uw reactie ' . $this->comment['body'] . ' is veranderd naar ' . $this->body,
+                'bg' => 'bg-green-200',
+                'border' => 'border-green-600'
+            ]);
+
+        } elseif($this->type == 'question') {
+
+            if (auth()->id() != $this->comment['user_id']) {
+                return redirect('/vraag/' . $this->slug);
+            }
+
+            $this->validate();
+
+            question_comments::where('id', $this->comment['id'])->
+            update(['body' => $this->body]);
+
+            return redirect('/vraag/' . $this->slug)->with([
+                'title' => 'Gelukt!',
+                'message' => 'Uw reactie ' . $this->comment['body'] . ' is veranderd naar ' . $this->body,
+                'bg' => 'bg-green-200',
+                'border' => 'border-green-600'
+            ]);
         }
-
-        $this->validate();
-
-        Comments::where('id', $this->comment['id'])->
-        update(['body' => $this->body]);
-
-        return redirect('/artikel/' . $this->slug)->with([
-            'title' => 'Gelukt!',
-            'message' => 'Uw reactie ' . $this->comment['body'] . ' is veranderd naar ' . $this->body,
-            'bg' => 'bg-green-200',
-            'border' => 'border-green-600'
-        ]);
 
     }
 
--- resources/views/livewire/vraag-bekijken.blade.php
+++ resources/views/livewire/vraag-bekijken.blade.php
@@ -131,7 +131,7 @@
                         <div class="flex flex-row items-center justify-center">
                             @if($comment->user->id == auth()->id())
                                 <button
-                                    wire:click='$emit("openModal", "reactie-wijzigen", {{ json_encode(["comment" => $comment, 'slug' => $question->slug, "type" => "article"]) }})'>
+                                    wire:click='$emit("openModal", "reactie-wijzigen", {{ json_encode(["comment" => $comment, 'slug' => $question->slug, "type" => "question"]) }})'>
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
                                          fill="gray">
                                         <path
@@ -143,7 +143,7 @@
                                 </button>
 
                                 <button
-                                    wire:click='$emit("openModal", "reactie-verwijderen", {{ json_encode(["comment" => $comment, 'slug' => $question->slug, "type" => "article"]) }})'>
+                                    wire:click='$emit("openModal", "reactie-verwijderen", {{ json_encode(["comment" => $comment, 'slug' => $question->slug, "type" => "question"]) }})'>
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" viewBox="0 0 20 20"
                                          fill="gray">
                                         <path fill-rule="evenodd"
@@ -236,6 +236,8 @@
         </div>
     </div>
 </div>
+
+
 
 
 

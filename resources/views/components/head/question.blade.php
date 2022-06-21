0948c403cb9a20033742dc545a6c13844137e9cf
ramonsimon <ramonsimon8@gmail.com>
2022-06-21 12:45:56 +0200

    Heroku fix

--- resources/views/components/head/question.blade.php
+++ resources/views/components/head/question.blade.php
@@ -1,4 +1,4 @@
-<script defer src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
+<script src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
 <div
     x-data="{ value: @entangle($attributes->wire('model')) }"
     x-init="
